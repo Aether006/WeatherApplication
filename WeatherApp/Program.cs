67768836b07f1fab402ffaf17a07989f6dc99cb9
Thomas Woolley <thomaswoolley22@icloud.com>
2024-07-09 19:10:46 +0100

    more changes

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace WeatherApp
+﻿using Newtonsoft.Json;
+using System.Dynamic;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace WeatherApp
 {
     internal class Program
     {
+        string apiKey = "91274abeabcd1ff8b14dc582b7d94e96";
+        public string city;
+
         static void Main(string[] args)
         {
-            string apiKey;
+            Console.WriteLine("Enter a city to view weather");
+            
+        }
+
+        void GetWeather()
+        {
+            using (WebClient client = new WebClient())
+            {
+                string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q=london&appid=91274abeabcd1ff8b14dc582b7d94e96", city , apiKey);
+                var json = client.DownloadString(url);
+                WeatherInfo.root Info = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+                
+                
+                
+            }
+
         }
     }
 }
